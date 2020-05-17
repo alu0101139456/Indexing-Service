29892c1b0a51260ec5f70fa9c7c8f862b7bf017a
Ángel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-05-17 20:28:21 +0100

    update

---
+++ indexing-service/actors/actorthread.cpp
@@ -0,0 +1,6 @@
+#include "actorthread.h"
+
+ActorThread::ActorThread()
+{
+
+}
---
+++ indexing-service/actors/actorthread.h
@@ -0,0 +1,13 @@
+#ifndef ACTORTHREAD_H
+#define ACTORTHREAD_H
+
+#include <QObject>
+
+class ActorThread
+{
+    Q_OBJECT
+public:
+    ActorThread();
+};
+
+#endif // ACTORTHREAD_H
